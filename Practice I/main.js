844263e2db88049de738567d0aacfa9efc085bae
Nour Batniji <81929344+nourbatniji@users.noreply.github.com>
2025-10-16 12:02:45 +0300

    Add files via upload

---
+++ Practice I/main.js
@@ -0,0 +1,37 @@
+//1
+let favAnimal = "Cat";
+console.log(favAnimal);
+
+//2
+let a = 9;
+let b = 8;
+let sum = a + b;
+console.log(sum);
+
+//3
+let age = 21;
+console.log("I am " + age + " years old!");
+
+//4
+let country = "Plaestine";
+country = "Turkey";
+console.log(country);
+
+//5
+const pi = 3.14;
+console.log(pi);
+
+//6
+let favoriteColor = "White"
+console.log(favoriteColor);
+
+//7 
+let x = 5;
+let y = 10;
+let SUM = x + y;
+console.log(SUM);
+
+//8
+let isSunny = true;
+console.log("It is sunny today");
+
